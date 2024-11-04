61fff347184ed313d0800de06e18d3ed24966a63
kanghyun.lee <kanghyun.lee@weedscomm.com>
2024-11-04 17:00:15 +0900

    Chore: 버튼 동작 추가

--- app/src/main/java/com/app/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/app/ui/fragment/MainFragment.kt
@@ -1,22 +1,39 @@
 package com.app.ui.fragment
 
+import android.view.Gravity
+import android.view.View
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
+import com.app.R
 import com.app.databinding.FragmentMainBinding
 import com.core.ui.BaseFragment
 import com.presentation.MainFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
+class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate),
+    View.OnClickListener {
     private val viewModel: MainFragmentViewModel by viewModels()
 
     override fun setUi() {
+        binding.viewModel = viewModel
+        binding.buttonOpenDrawerlayout.setOnClickListener(this)
+        binding.buttonAddImg.setOnClickListener(this)
+        binding.buttonMenu.setOnClickListener(this)
     }
 
     override fun setUpDate() {
     }
 
     override fun setObserve(lifecycleOwner: LifecycleOwner) {
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_open_drawerlayout -> binding.drawerLayout.openDrawer(GravityCompat.START)
+            R.id.button_add_img ->{}
+            R.id.button_menu ->{}
+        }
     }
 }
