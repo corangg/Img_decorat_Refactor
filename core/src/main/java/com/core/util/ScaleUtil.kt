363cbe44f2bfd5b42ec1e34b4f9c0d60b4b93338
kanghyun.lee <kanghyun.lee@weedscomm.com>
2024-11-07 15:02:41 +0900

    Add: getScaleParams 추가 Chore: 배경 위치 조정

--- core/src/main/java/com/core/util/ScaleUtil.kt
+++ core/src/main/java/com/core/util/ScaleUtil.kt
@@ -2,17 +2,12 @@ package com.core.util
 
 import android.view.ViewGroup.LayoutParams
 
-fun LayoutParams.getScaleParams(screenWith: Int, scale: Float): LayoutParams {
-    return if (scale > 1) {
-        this.apply {
-            width = screenWith
-            height = (screenWith / scale).toInt()
-        }
+fun LayoutParams.getScaleParams(screenWith: Int, scale: Float) = apply {
+    if (scale > 1) {
+        width = screenWith
+        height = (screenWith / scale).toInt()
     } else {
-        this.apply {
-            width = (screenWith * scale).toInt()
-            height = screenWith
-        }
+        width = (screenWith * scale).toInt()
+        height = screenWith
     }
-
 }
