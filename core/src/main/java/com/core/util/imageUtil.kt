efa48c992fc0e575a1e3a8761b9894fd59e64e94
kang <dlwprkdlq@naver.com>
2025-02-04 03:51:15 +0900

    Add: bitmapToUri 추가

--- core/src/main/java/com/core/util/imageUtil.kt
+++ core/src/main/java/com/core/util/imageUtil.kt
@@ -8,10 +8,16 @@ import android.net.Uri
 import android.view.View
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import java.io.File
 import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.io.InputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-fun Context.uriToBitmap(imageUri: Uri): Bitmap? {
+fun Context.uriToBitmap(imageUri: Uri?): Bitmap? {
+    imageUri?: return null
     var inputStream: InputStream? = null
     return try {
         inputStream = contentResolver.openInputStream(imageUri)
@@ -22,6 +28,23 @@ fun Context.uriToBitmap(imageUri: Uri): Bitmap? {
     } finally {
         inputStream?.close()
     }
+}
+
+fun Context.bitmapToUri( bitmap: Bitmap?): Uri? {
+    bitmap?: return null
+    val cachePath = File(cacheDir, "images")
+    cachePath.mkdirs()
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+    val fileName = "temp_image_$timeStamp.png"
+    val file = File(cachePath, fileName)
+    FileOutputStream(file).use { out ->
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+    }
+    return androidx.core.content.FileProvider.getUriForFile(
+        this,
+        "${packageName}.fileprovider",
+        file
+    )
 }
 
 fun View.backgroundTarget() = object : CustomTarget<Drawable>() {
