2f969992b74c0fe02e5216d9cac79eaa3e58b2de
kang <dlwprkdlq@naver.com>
2025-02-25 19:21:33 +0900

    Add : Emoji 클릭시 이미지 레이어에 추가되는 기능 구현

--- app/src/main/java/com/app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/app/ui/activity/MainActivity.kt
@@ -93,7 +93,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
                     true
                 }
 
-                R.id.menu_navi_background, R.id.menu_navi_hue, R.id.menu_navi_sticker -> {
+                R.id.menu_navi_background, R.id.menu_navi_hue, R.id.menu_navi_sticker, R.id.menu_navi_text -> {
                     NavigationUI.onNavDestinationSelected(item, navController)
                     true
                 }
--- presentation/src/main/java/com/presentation/StickerViewModel.kt
+++ presentation/src/main/java/com/presentation/StickerViewModel.kt
@@ -6,7 +6,6 @@ import com.core.di.DefaultDispatcher
 import com.core.di.IoDispatcher
 import com.core.di.MainDispatcher
 import com.core.viewmodel.BaseViewModel
-import com.domain.usecase.AddViewImageItemData
 import com.domain.usecase.DownloadEmojiUseCase
 import com.domain.usecase.GetEmojiUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -21,10 +20,10 @@ class StickerViewModel @Inject constructor(
     @MainDispatcher mainDispatcher: MainCoroutineDispatcher,
     @DefaultDispatcher defaultDispatcher: CoroutineDispatcher,
     @IoDispatcher ioDispatcher: CoroutineDispatcher
-) : BaseViewModel(mainDispatcher, defaultDispatcher, ioDispatcher){
+) : BaseViewModel(mainDispatcher, defaultDispatcher, ioDispatcher) {
     val emojiData = getEmojiUseCase().asLiveData(viewModelScope.coroutineContext)
 
-    fun downloadEmojiList() = onIoWork{
+    fun downloadEmojiList() = onIoWork {
         downloadEmojiUseCase()
     }
 }
