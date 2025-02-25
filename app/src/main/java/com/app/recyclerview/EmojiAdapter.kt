eea7e0db268f26aac0cc078aa4f3eecf585f7174
kang <dlwprkdlq@naver.com>
2025-02-25 18:16:55 +0900

    Add : EmojiFragment 내용 추가

---
+++ app/src/main/java/com/app/recyclerview/EmojiAdapter.kt
@@ -0,0 +1,37 @@
+package com.app.recyclerview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import com.app.databinding.ItemEmojiBinding
+import com.core.recyclerview.BaseRecyclerView
+import com.core.recyclerview.BaseViewHolder
+
+class EmojiAdapter : BaseRecyclerView<String, EmojiAdapter.EmojiViewHolder>(object :
+    DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String) =
+        oldItem.hashCode() == newItem.hashCode()
+
+    override fun areContentsTheSame(oldItem: String, newItem: String) = oldItem == newItem
+}) {
+    private lateinit var mContext: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmojiViewHolder {
+        mContext = parent.context
+        return EmojiViewHolder(
+            ItemEmojiBinding.inflate(
+                LayoutInflater.from(mContext), parent, false
+            )
+        )
+    }
+
+    inner class EmojiViewHolder(
+        private val binding: ItemEmojiBinding
+    ) : BaseViewHolder<String>(binding) {
+        override fun bind(item: String, position: Int, clickListener: ((String, Int) -> Unit)?) {
+            binding.emojiItem.text = item
+            itemView.setOnClickListener { clickListener?.invoke(item, position) }
+        }
+    }
+}
--- app/src/main/java/com/app/ui/fragment/sticker/EmojiFragment.kt
+++ app/src/main/java/com/app/ui/fragment/sticker/EmojiFragment.kt
@@ -1,60 +1,36 @@
 package com.app.ui.fragment.sticker
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.app.R
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.GridLayoutManager
+import com.app.databinding.FragmentEmojiBinding
+import com.app.recyclerview.EmojiAdapter
+import com.core.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class EmojiFragment(private val list: List<String>) : BaseFragment<FragmentEmojiBinding>(FragmentEmojiBinding::inflate) {
+    //private val viewModel: HueViewModel by viewModels()
+    private val adapter by lazy { EmojiAdapter() }
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EmojiFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class EmojiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun setUi() {
+        binding.emojiRecycle.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), 8, GridLayoutManager.VERTICAL, false)
+            adapter = this@EmojiFragment.adapter
         }
+
+        adapter.setOnItemClickListener { item, position ->
+            position
+            item
+            true
+        }
+        adapter.submitList(list)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emoji, container, false)
+    override fun setUpDate() {
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EmojiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EmojiFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun setObserve(lifecycleOwner: LifecycleOwner) {
     }
+
 }
