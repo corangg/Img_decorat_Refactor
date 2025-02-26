4aa76e3cb62073f6329f2b741f8c5e421654f9d7
kang <dlwprkdlq@naver.com>
2025-02-26 12:14:01 +0900

    Add: 글자색, 글자 배경색 관련 프래그먼트, 리사이클러뷰 추가

---
+++ app/src/main/java/com/app/recyclerview/TextColorAdapter.kt
@@ -0,0 +1,69 @@
+package com.app.recyclerview
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import com.app.R
+import com.app.databinding.ItemBackgroundColorBinding
+import com.app.databinding.ItemTextColorBinding
+import com.app.recyclerview.BackgroundColorAdapter.BackgroundColorViewHolder
+import com.core.recyclerview.BaseRecyclerView
+import com.core.recyclerview.BaseViewHolder
+
+class TextColorAdapter : BaseRecyclerView<Int, TextColorAdapter.TextColorViewHolder>(object :
+    DiffUtil.ItemCallback<Int>() {
+    override fun areItemsTheSame(
+        oldItem: Int,
+        newItem: Int
+    ) = oldItem.hashCode() == newItem.hashCode()
+
+    override fun areContentsTheSame(
+        oldItem: Int,
+        newItem: Int
+    ) = oldItem == newItem
+}) {
+    private lateinit var mContext: Context
+    private var selectedPosition = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextColorViewHolder {
+        mContext = parent.context
+        return TextColorViewHolder(
+            ItemTextColorBinding.inflate(
+                LayoutInflater.from(mContext), parent, false
+            )
+        )
+    }
+
+    inner class TextColorViewHolder(private val binding : ItemTextColorBinding) : BaseViewHolder<Int>(binding){
+        private val border = GradientDrawable().apply {
+            setStroke(8, ContextCompat.getColor(mContext, R.color.point_color))
+        }
+
+        override fun bind(
+            item: Int,
+            position: Int,
+            clickListener: ((Int, Int) -> Unit)?
+        ) {
+
+            if (position == selectedPosition) {
+                binding.textColorItem.background = border.apply {
+                    setColor(item)
+                }
+            } else {
+                binding.textColorItem.setBackgroundColor(item)
+            }
+
+            itemView.setOnClickListener {
+                val previousPosition = selectedPosition
+                selectedPosition = position
+
+                notifyItemChanged(previousPosition)
+                notifyItemChanged(selectedPosition)
+                clickListener?.invoke(item, position)
+            }
+        }
+    }
+}
--- app/src/main/java/com/app/ui/fragment/text/TextColorFragment.kt
+++ app/src/main/java/com/app/ui/fragment/text/TextColorFragment.kt
@@ -1,60 +1,42 @@
 package com.app.ui.fragment.text
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.app.R
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.app.config.colorsList
+import com.app.databinding.FragmentTextColorBinding
+import com.app.recyclerview.TextColorAdapter
+import com.core.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class TextColorFragment :
+    BaseFragment<FragmentTextColorBinding>(FragmentTextColorBinding::inflate) {
+    private val textColorAdapter by lazy { TextColorAdapter() }
+    private val textBackgroundColorAdapter by lazy { TextColorAdapter() }
+    override fun setUi() {
+        setRecyclerView(binding.recyclerTextColor, textColorAdapter)
+        setRecyclerView(binding.recyclerTextBackgroundColor, textBackgroundColorAdapter)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TextColorFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TextColorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        textColorAdapter.setOnItemClickListener { item, position ->
+        }
+        textBackgroundColorAdapter.setOnItemClickListener { item, position ->
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_color, container, false)
+    override fun setUpDate() {
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TextColorFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TextColorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun setObserve(lifecycleOwner: LifecycleOwner) {
+
+    }
+
+    private fun setRecyclerView(view: RecyclerView, adapter: TextColorAdapter) {
+        view.apply {
+            layoutManager = GridLayoutManager(requireContext(), 1, GridLayoutManager.HORIZONTAL, false)
+            this.adapter = adapter
+        }
+        adapter.submitList(colorsList)
     }
 }
